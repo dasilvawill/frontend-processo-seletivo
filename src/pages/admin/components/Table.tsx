cf721e58fc4420ceaa338f9c4a92a982a2d4a84b
william.silva <william.silva@acad.ftec.com.br>
2022-12-08 09:59:53 -0300

    ajuste que remove excel quebra pacote

--- src/pages/admin/components/Table.tsx
+++ src/pages/admin/components/Table.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import { api } from "../../../services/api"
 import { Switch } from "@material-tailwind/react"
-import ReactHTMLTableToExcel from "react-html-table-to-excel"
+// import ReactHTMLTableToExcel from "react-html-table-to-excel"
 
 export default function Table() {
   const [coursesData, setCoursesData] = useState<any>()
@@ -20,13 +20,13 @@ export default function Table() {
       <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
           <div className="flex justify-end mb-4 mt-2">
-            <ReactHTMLTableToExcel
+            {/* <ReactHTMLTableToExcel
               className="max-w-screen-sm w-[10vw] bg-blue-200 shadow-md shadow-gray-400 dark:shadow-gray-900 text-gray-800 font-medium rounded-2xl p-3"
               table="emp-table"
               filename="lista-de-cursos"
               sheet="Sheet"
               buttonText="Exportar para Excel"
-            />
+            /> */}
           </div>
           <div className="overflow-hidden">
             <table className="min-w-full border-2" id="emp-table">
